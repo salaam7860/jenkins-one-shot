f4ca3e2343bde60cbf4d7aa55a5304603e010a47
saad zaheer <salaam7860@gmail.com>
2025-03-26 15:24:28 +0500

    add groovy file

---
+++ jenkinsJobs/learning/uptoPush.groovy
@@ -0,0 +1,44 @@
+pipeline {
+    
+    agent {label 'agentx'}
+    
+    stages {
+        
+        stage('Code') {
+            steps {
+                echo 'This is coping the code.'
+                git url: "https://github.com/salaam7860/django-notes.git", branch:"main"
+                echo "The code has been successfully cloned"
+            }
+        }
+        
+        stage('Build') {
+            steps {
+                echo 'This is building the code'
+                sh 'whoami'
+                sh 'docker build -t notes-app:latest .'
+            }
+            
+        }
+        
+        stage('Push to docker-hub') {
+            steps {
+                echo 'This is pushing the image to dockerhub'
+                withCredentials([usernamePassword(credentialsId: "dockerHubCred", passwordVariable: "dockerHubPass", usernameVariable: "dockerHubUser")]) {
+                    sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+                    sh "docker image tag notes-app:latest ${dockerHubUser}/notes-app:latest"
+                    sh "docker push ${dockerHubUser}/notes-app:latest"
+              
+                }
+            }
+        }
+        
+        stage('Deploy') {
+            steps {
+                echo 'This is deploying the code.'
+                sh 'docker-compose up -d'
+            }
+        }
+        
+    }
+}
